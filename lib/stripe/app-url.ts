aca69e403ad4f593dce87d7cdb58f7a57bef9c65
Anna Zoi <zoianna01@gmail.com>
2026-05-30 21:20:58 +0300

    app_url

--- lib/stripe/app-url.ts
+++ lib/stripe/app-url.ts
@@ -1,9 +1,4 @@
-import { environments } from '@/config/environments';
-
 export function getAppUrl(request?: Request) {
-	if (environments.NEXT_PUBLIC_APP_URL) {
-		return environments.NEXT_PUBLIC_APP_URL.replace(/\/$/, '');
-	}
 	if (request) {
 		const origin = request.headers.get('origin');
 		if (origin) return origin.replace(/\/$/, '');
